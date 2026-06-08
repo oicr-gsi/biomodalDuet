fa86ec3a2fb9bd74c01ac4d3487f395710d61aff
Gavin Peng <gpeng@oicr.on.ca>
2026-06-08 10:36:33 -0400

    add more checks to regression test

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -6,3 +6,9 @@ set -o pipefail
 cd "$1"
 
 ls | sed 's/.*\.//' | sort | uniq -c
+cat test_01.metrics_definitions.csv | sort | md5sum
+cat test_01.summary.csv | sort | md5sum
+zcat test_01.hmc_cxreport.txt.gz | sort | md5sum
+zcat test_01.mc_cxreport.txt.gz | sort | md5sum
+zcat test_01.modc_cxreport.txt.gz | sort | md5sum
+zcat test_01.vcf.gz | grep -v "^#" | sort | md5sum
